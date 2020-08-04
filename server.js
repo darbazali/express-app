cb2b40507fc6c4c9f446d5cae96f05ef76e5acea
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-04 16:09:05 +0300

    serve HTML file

--- server.js
+++ server.js
@@ -1,15 +1,56 @@
 const express = require('express');
 const app = express()
 
-
-
 const port = 3000;
 
 
+// --> 7)  Mount the Logger middleware here
+
+
+// --> 11)  Mount the body-parser middleware  here
+
+
+/** 1) Meet the node console. */
+console.log('Hello World')
+
+/** 2) A first working Express Server */
 app.get('/', (req, res) => {
-    // res.send(`<h1>Hello World, I'm a Node server listening on port ${port}</h1>`)
-    res.send('Hello World')
+    res.send('Hello ExpresssssEEEE')
 })
+
+/** 3) Serve an HTML file */
+app.get('/home', (req, res) => {
+    res.sendFile( __dirname + "/index.html")
+})
+
+/** 4) Serve static assets  */
+
+
+/** 5) serve JSON on a specific route */
+
+
+/** 6) Use the .env file to configure the app */
+ 
+ 
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+
+
+/** 8) Chaining middleware. A Time server */
+
+
+/** 9)  Get input from client - Route parameters */
+
+
+/** 10) Get input from client - Query parameters */
+// /name?first=<firstname>&last=<lastname>
+
+  
+/** 11) Get ready for POST Requests - the `body-parser` */
+// place it before all the routes !
+
+
+/** 12) Get data form POST  */
 
 
 app.listen( port, () => {
