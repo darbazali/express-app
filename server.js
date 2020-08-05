74eb3f9b0d5e65ae1495f0c5b9e05533f4834189
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-05 17:38:36 +0300

    working with environment variables

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const dotenv = require('dotenv')
+dotenv.config();
 
 const app = express();
 const hostName = 'localhost';
 const port = 3000;
+
+// process.env.MESSAGE_STYEL='uppercase';
 
 // creat a simple server, send response back to home dir.
 app.get('/', (req, res) => {
@@ -25,11 +29,18 @@ app.use( express.static(__dirname + '/public/'))
 /*==================================
     5) serve JSON on a specific route
 ===================================*/
-app.get('/api', (req, res) => {
-    res.json({name: 'Darbaz Ali'})
+
+
+app.get('/api', (req, res) => { 
+    var message = process.env.MESSAGE_STYLE;
+    if ( message === 'uppercase') {
+        res.json({'message': 'hello json'.toUpperCase()})
+    } else { 
+        res.json({'message': 'hello json'})
+    }
 })
 
 // listen for requestes at http://localhost:3000
 app.listen(port, hostName, () => {
     console.log(`Server running at http://${hostName}:${port}`)
-})
+}) 
