7e79492fd8eeb3be28fc9e91b2873e3df7c789d1
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-09 11:03:20 +0300

    serve static assets

--- App.js
+++ App.js
@@ -3,15 +3,26 @@ const app = express();
 const hostname = 'localhost';
 const port = 3000;
 
-// setup a server to listn for home url
+/*================================== 
+    2. setup a server to listn for home url
+==================================*/
 app.get('/hello', (req, res) => {
     res.send('Hello world')
+    console.log('hello');
 })
 
-// Serve HTML file
+
+/*================================== 
+    3. Serve HTML file
+==================================*/
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
+
+/*================================== 
+    3. Serve Static assets
+==================================*/
+app.use(express.static(__dirname + '/public/'))
 
 // setup server on port and hostname
 app.listen(port, hostname, () => {
